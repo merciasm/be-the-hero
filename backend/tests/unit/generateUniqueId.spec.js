036ad871b224e72b3954ce63e255258040e8881c
Mércia Maguerroski <merciasmc@outlook.com>
2020-08-15 11:24:43 -0300

    test

--- backend/tests/unit/generateUniqueId.spec.js
+++ backend/tests/unit/generateUniqueId.spec.js
@@ -5,11 +5,3 @@ describe('Generate Unique ID', () => {
         expect(id).toHaveLength(8)
     })
 })
-
-const generateId = require('../../src/utils/generateUniqueId')
-describe('Generate Unique ID', () => {
-    it('should generate an unique ID', () => {
-        const id = generateId()
-        expect(id).toHaveLength(8)
-    })
-})
